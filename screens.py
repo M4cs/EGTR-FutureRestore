0bbd20a614f7b460d2ddf75ce61ef25ed6ef28fe
M4cs <maxlikescs@gmail.com>
2019-01-31 16:41:22 -0500

    Fix Bugs

--- screens.py
+++ screens.py
@@ -57,17 +57,18 @@ def mainScreen():
     while True:
         event, values = window.Read()
         if event == 'Exit':
-            exit()
+            window.Close()
+            break
         elif event == 'Donate':
             webbrowser.open_new_tab('https://paypal.me/m4csdev')
         elif event == 'Start':
             if values['_LATESTSEP_'] == True:
-                latestsep = '--latest-sep'
+                latestsep = ' --latest-sep'
                 sep_path = ''
             elif values['_LATESTSEP_'] == False:
                 latestsep = ''
             if values['_LATESTBASE_'] == True:
-                latestbase = '--latest-baseband'
+                latestbase = ' --latest-baseband'
                 base_path = ''
             elif values['_LATESTBASE_'] == False:
                 latestbase = ''
@@ -76,10 +77,10 @@ def mainScreen():
                     [[p.T('Error: You must enter an IPSW! ')]]
                 ).Read()
             elif values['_IPSW_'] != '':
-                ipsw_path = getRealPath(values['_IPSW_'])
+                ipsw_path = " " + getRealPath(values['_IPSW_'])
             if values['_SEP_'] == '':
                 if values['_LATESTSEP_'] == True:
-                    latestsep = '--latest-sep'
+                    latestsep =  '--latest-sep'
                     sep_path = ''
                 elif values['_LATESTSEP_'] == False:
                     p.Window('Error', auto_close=True, auto_close_duration=3, keep_on_top=True, no_titlebar=True, grab_anywhere=True).Layout(
@@ -88,14 +89,14 @@ def mainScreen():
             elif values['_SEP_'] != '':
                 if values['_LATESTSEP_'] == True:
                     sep_path = ''
-                    latestsep = '--latest-sep'
+                    latestsep = ' --latest-sep'
                 elif values['_LATESTSEP_'] == False:
-                    sep_path = '-s ' + getRealPath(values['_SEP_'])
+                    sep_path = ' -s ' + getRealPath(values['_SEP_'])
                     latestsep = ''
             if values['_BASE_'] == '':
                 if values['_LATESTBASE_'] == True:
                     base_path = ''
-                    latestbase = '--latest-base'
+                    latestbase = ' --latest-base'
                 elif values['_LATESTBASE_'] == False:
                     p.Window('Error', auto_close=True, auto_close_duration=3, keep_on_top=True, no_titlebar=True, grab_anywhere=True).Layout(
                         [[p.T('Error: You must enter an Base path or choose Use Latest! ')]]
@@ -103,48 +104,48 @@ def mainScreen():
             elif values['_BASE_'] != '':
                 if values['_LATESTBASE_'] == True:
                     base_path = ''
-                    latestbase = '--latest-base'
+                    latestbase = ' --latest-base'
                 elif values['_LATESTBASE_'] == False:
-                    base_path = '-b ' + getRealPath(values['_BASE_'])
+                    base_path = ' -b ' + getRealPath(values['_BASE_'])
                     latestbase = ''
             if values['_BLOBS_'] == '':
                 p.Window('Error', auto_close=True, auto_close_duration=3, keep_on_top=True, no_titlebar=True, grab_anywhere=True).Layout(
                         [[p.T('Error: You must choose SHSH2 Blobs! ')]]
                     ).Read()
             elif values['_BLOBS_'] != '':
-                blobs_path = '-t ' + getRealPath(values['_BLOBS_'])
+                blobs_path = ' -t' + getRealPath(values['_BLOBS_'])
             if values['_DEBUG_'] == True:
-                debug = '-d'
+                debug = ' -d'
             elif values['_DEBUG_'] == False:
                 debug = ''
             if values['_BASEMANI_'] == '':
                 basemani = ''
             elif values['_BASEMANI_'] != '':
-                basemani = '-p ' + values['_BASEMANI_']
+                basemani = ' -p' + values['_BASEMANI_']
             if values['_SEPMANI_'] == '':
                 sepmani = ''
             elif values['_SEPMANI_'] != '':
-                sepmani = '-m ' + values['_SEPMANI_']
+                sepmani = ' -m ' + values['_SEPMANI_']
             if values['_NOBASEBAND_'] == True:
-                nobaseband = '--no-baseband'
+                nobaseband = ' --no-baseband'
                 base_path = ''
                 basemani = ''
             elif values['_NOBASEBAND_'] == False:
                 nobaseband = ''
             if values['_UPDATE_'] == True:
-                update = '-u'
+                update = ' -u'
             elif values['_UPDATE_'] == False:
                 update = ''
             if values['_WAIT_'] == True:
-                wait = '-w'
+                wait = ' -w'
             elif values['_WAIT_'] == False:
                 wait = ''
-                
             if getTypeFutureRestore() == 1:
                 futurerestore = getRealPath(DOWNLOAD_DIRECTORY + '/futurerestore')
             elif getTypeFutureRestore() == 2:
                 futurerestore = getRealPath(DOWNLOAD_DIRECTORY + '/futurerestore.exe')
-            query = futurerestore + " " + blobs_path + " " + base_path + " " + sep_path + latestbase + " " + latestsep + " " + debug + " " + basemani + " " + sepmani + " " + " " + update + " " + nobaseband + " " + wait + " " + ipsw_path
+            query = futurerestore + blobs_path + base_path + sep_path + latestbase + latestsep + debug + basemani + sepmani + update + nobaseband + wait + ipsw_path
+            print(query)
             outputscreen = p.Window('Logs:', no_titlebar=True, keep_on_top=True, grab_anywhere=True).Layout(
                 [[p.T('Are You Sure? You may risk bootlooping or bricking your device! ')],
                 [p.Button('Cancel'), p.Button('Continue')]])
@@ -154,4 +155,9 @@ def mainScreen():
                     p.Window('Logs:', no_titlebar=True, keep_on_top=True, grab_anywhere=True, auto_close=True, auto_close_duration=5).Layout(
                 [[p.T('Refer To The Terminal For Output ')]]).Read()
                     os.system(query)
+                    break
+                elif event == 'Cancel':
+                    outputscreen.Close()
+                    break
+    window.Close()
             
